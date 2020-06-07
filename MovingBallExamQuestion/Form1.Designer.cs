a7731fcc49d89e12cf6bc3ca31c1a545b49e42ed
Rezo Megrelidze <rezomegrelidze@gmail.com>
2020-06-07 20:55:03 +0300

    Add project files.

---
+++ MovingBallExamQuestion/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MovingBallExamQuestion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.basketball = new System.Windows.Forms.PictureBox();
+            this.upPb = new System.Windows.Forms.PictureBox();
+            this.leftPb = new System.Windows.Forms.PictureBox();
+            this.rightPb = new System.Windows.Forms.PictureBox();
+            this.downPb = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.basketball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upPb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downPb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.basketball);
+            this.panel1.Location = new System.Drawing.Point(637, 128);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(676, 508);
+            this.panel1.TabIndex = 4;
+            // 
+            // basketball
+            // 
+            this.basketball.Image = ((System.Drawing.Image)(resources.GetObject("basketball.Image")));
+            this.basketball.Location = new System.Drawing.Point(100, 103);
+            this.basketball.Name = "basketball";
+            this.basketball.Size = new System.Drawing.Size(113, 95);
+            this.basketball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.basketball.TabIndex = 0;
+            this.basketball.TabStop = false;
+            // 
+            // upPb
+            // 
+            this.upPb.Image = ((System.Drawing.Image)(resources.GetObject("upPb.Image")));
+            this.upPb.Location = new System.Drawing.Point(214, 146);
+            this.upPb.Name = "upPb";
+            this.upPb.Size = new System.Drawing.Size(144, 142);
+            this.upPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.upPb.TabIndex = 5;
+            this.upPb.TabStop = false;
+            this.upPb.Click += new System.EventHandler(this.upPb_Click);
+            // 
+            // leftPb
+            // 
+            this.leftPb.Image = ((System.Drawing.Image)(resources.GetObject("leftPb.Image")));
+            this.leftPb.Location = new System.Drawing.Point(52, 307);
+            this.leftPb.Name = "leftPb";
+            this.leftPb.Size = new System.Drawing.Size(143, 141);
+            this.leftPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.leftPb.TabIndex = 6;
+            this.leftPb.TabStop = false;
+            this.leftPb.Click += new System.EventHandler(this.leftPb_Click);
+            // 
+            // rightPb
+            // 
+            this.rightPb.Image = ((System.Drawing.Image)(resources.GetObject("rightPb.Image")));
+            this.rightPb.Location = new System.Drawing.Point(375, 305);
+            this.rightPb.Name = "rightPb";
+            this.rightPb.Size = new System.Drawing.Size(143, 143);
+            this.rightPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.rightPb.TabIndex = 7;
+            this.rightPb.TabStop = false;
+            this.rightPb.Click += new System.EventHandler(this.rightPb_Click);
+            // 
+            // downPb
+            // 
+            this.downPb.Image = ((System.Drawing.Image)(resources.GetObject("downPb.Image")));
+            this.downPb.Location = new System.Drawing.Point(210, 464);
+            this.downPb.Name = "downPb";
+            this.downPb.Size = new System.Drawing.Size(145, 141);
+            this.downPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.downPb.TabIndex = 8;
+            this.downPb.TabStop = false;
+            this.downPb.Click += new System.EventHandler(this.downPb_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1396, 1012);
+            this.Controls.Add(this.downPb);
+            this.Controls.Add(this.rightPb);
+            this.Controls.Add(this.leftPb);
+            this.Controls.Add(this.upPb);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.basketball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upPb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downPb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox basketball;
+        private System.Windows.Forms.PictureBox upPb;
+        private System.Windows.Forms.PictureBox leftPb;
+        private System.Windows.Forms.PictureBox rightPb;
+        private System.Windows.Forms.PictureBox downPb;
+    }
+}
+
---
+++ MovingBallExamQuestion/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingBallExamQuestion
+{
+    public partial class Form1 : Form
+    {
+        private int delta = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveBallDown()
+        {
+            basketball.Location =
+                new Point(basketball.Location.X , basketball.Location.Y + delta);
+        }
+
+        private void MoveBallUp()
+        {
+            basketball.Location =
+                new Point(basketball.Location.X , basketball.Location.Y - delta);
+        }
+
+        private void MoveBallRight()
+        {
+            basketball.Location =
+                new Point(basketball.Location.X + delta, basketball.Location.Y); ;
+        }
+
+        private void MoveBallLeft()
+        {
+            basketball.Location = 
+                new Point(basketball.Location.X - delta,basketball.Location.Y);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            var key = e.KeyCode;
+            if(key == Keys.Right) MoveBallRight();
+            else if(key == Keys.Left) MoveBallLeft();
+            else if(key == Keys.Up) MoveBallUp();
+            else if(key == Keys.Down) MoveBallDown();
+        }
+
+        private void upPb_Click(object sender, EventArgs e)
+        {
+            MoveBallUp();
+        }
+
+        private void leftPb_Click(object sender, EventArgs e)
+        {
+            MoveBallLeft();
+        }
+
+        private void rightPb_Click(object sender, EventArgs e)
+        {
+            MoveBallRight();
+        }
+
+        private void downPb_Click(object sender, EventArgs e)
+        {
+            MoveBallDown();
+        }
+    }
+}
